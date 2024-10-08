7da9fe4667efecf56d0eedf92623f7cb3c99d55b
oe.marcos <marcos.ojeda.escalante@gmail.com>
2024-10-08 13:17:14 -0300

    crud evitando inyección SQL

--- crudEstudiantes.js
+++ crudEstudiantes.js
@@ -10,28 +10,44 @@ const findAll = async () => {
 const findByRut = async (rut) => {
     const pool = conexion();
     await pool.connect();
-    const estudiantes = await pool.query(`SELECT * FROM estudiante WHERE rut = '${rut}'`);
+    const query = {
+        text: 'SELECT * FROM estudiante WHERE rut = $1',
+        values: [rut]
+    }
+    const estudiantes = await pool.query(query);
     console.log(estudiantes);
 }
 
 const insert = async (rut, nombre, curso, nivel) => {
     const pool = conexion();
     await pool.connect();
-    const estudiantes = await pool.query(`INSERT INTO estudiante(rut, nombre, curso, nivel) VALUES ('${rut}', '${nombre}', '${curso}', ${nivel})`);
+    const query = {
+        text: 'INSERT INTO estudiante(rut, nombre, curso, nivel) VALUES ($1, $2, $3, $4)',
+        values: [rut, nombre, curso, nivel]
+    }
+    const estudiantes = await pool.query(query);
     console.log(estudiantes);
 }
 
 const update = async (rut, nombre, curso, nivel) => {
     const pool = conexion();
     await pool.connect();
-    const estudiantes = await pool.query(`UPDATE estudiante SET nombre = '${nombre}', curso = '${curso}', nivel = ${nivel} WHERE rut = '${rut}'`);
+    const query = {
+        text: 'UPDATE estudiante SET nombre = $1, curso = $2, nivel = $3 WHERE rut = $4',
+        values: [nombre, curso, nivel, rut]
+    }
+    const estudiantes = await pool.query(query);
     console.log(estudiantes);
 }
 
 const deleteByRut = async (rut) => {
     const pool = conexion();
     await pool.connect();
-    const estudiantes = await pool.query(`DELETE FROM estudiante WHERE rut = '${rut}'`);
+    const query = {
+        text: 'DELETE FROM estudiante WHERE rut = $1',
+        values: [rut]
+    }
+    const estudiantes = await pool.query(query);
     console.log(estudiantes);
 }
 
